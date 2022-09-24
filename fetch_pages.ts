34b0dea79d7b2256b9c3dbc190bf274170a1d2a1
jigsaw <m@jgs.me>
2022-09-24 10:59:44 +0900

    Fetch only updated

--- fetch_pages.ts
+++ fetch_pages.ts
@@ -1,8 +1,13 @@
 import pub from "./data/pub.json" assert { type: "json" };
 
+const last = new Date(parseInt(await Deno.readTextFile("./data/LAST")))
+  .getTime();
+
 let cnt = 0;
 
-for (const { title, id } of pub) {
+const updatedPages = pub.filter((page) => page.updated * 1000 > last);
+
+for (const { title, id } of updatedPages) {
   const res = await fetch(
     `https://scrapbox.io/api/pages/jigsaw/${encodeURIComponent(title)}`,
   );
