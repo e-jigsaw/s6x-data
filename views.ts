8668034f49645118e299dc0bef17439ef72ff62f
jigsaw <m@jgs.me>
2023-11-12 17:23:31 +0900

    add views script

---
+++ views.ts
@@ -0,0 +1,33 @@
+import { readFile, writeFile } from "fs/promises";
+import format from "date-fns/format";
+import subDays from "date-fns/subDays";
+import getHours from "date-fns/getHours";
+import master from "./data/pub.json";
+
+const PATH = "./data/COUNT";
+
+const main = async () => {
+  const prevCount = parseInt((await readFile(PATH)).toString());
+  const count = master.reduce((prev, curr) => (prev += curr.views), 0);
+  const diff = count - prevCount;
+  const headers = new Headers();
+  headers.append("X-USER-TOKEN", process.env.PIXE_LA_TOKEN as string);
+  const res = await fetch("https://pixe.la/v1/users/jgs/graphs/sbv", {
+    headers,
+    method: "POST",
+    body: JSON.stringify({
+      date: format(
+        subDays(new Date(), getHours(new Date()) === 19 ? 0 : 1),
+        "yyyyMMdd",
+      ),
+      quantity: diff.toString(),
+    }),
+  });
+  const json = await res.json();
+  console.log(json);
+  console.log(`Today views: ${diff}`);
+  console.log(`Total views: ${count}`);
+  await writeFile(PATH, count.toString());
+};
+
+main();
